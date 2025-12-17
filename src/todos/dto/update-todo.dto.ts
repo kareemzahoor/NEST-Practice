b9321bad8ddbc3d2067cb278a13f62413bfac8b2
Kareem Zahoor <kareemzahoor79@gmail.com>
2025-12-17 16:01:26 +0500

    Update update-todo.dto.ts

--- src/todos/dto/update-todo.dto.ts
+++ src/todos/dto/update-todo.dto.ts
@@ -1,9 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTodoDto } from './create-todo.dto';
-import { IsNotEmpty, IsUUID } from 'class-validator';
 
-export class UpdateTodoDto extends PartialType(CreateTodoDto) {
-  @IsUUID()
-  @IsNotEmpty()
-  id: string;
-}
+export class UpdateTodoDto extends PartialType(CreateTodoDto) {}
